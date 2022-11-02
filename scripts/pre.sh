bf7cf6377fac2760d6eb36024341fb8030229676
Potados <potados99@gmail.com>
2022-11-03 08:27:40 +0900

    chore: if 제거 ㅠ

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -7,21 +7,11 @@ set -e
 echo "호스트 파일(/etc/hosts)을 준비합니다."
 cp resources/system/hosts /etc/hosts
 
-if yum list installed "mariadb-server" >/dev/null 2>&1; then
-    echo "MariaDB를 설치합니다."
-    yum -y install mariadb-server
-else
-    echo "MariaDB가 이미 설치되어 있기 때문에 건너뜁니다."
-fi
+echo "MariaDB를 설치합니다."
+yum -y install mariadb-server
 
-if id "mha" &>/dev/null; then
-    echo "'mha' 사용자가 이미 존재하기 때문에 건너뜁니다."
-else
-    echo "시스템에서 사용할 mha 사용자를 생성합니다."
-    useradd -g mysql mha
+echo "시스템에서 사용할 mha 사용자를 생성합니다."
+useradd -g mysql mha || echo "앗, 'mha' 사용자가 이미 존재하나봅니다!"
 
-    read -p "'mha' 사용자에 사용할 비밀번호를 적어 주세요: " mha_password
-    echo "mha:$mha_password" | chpasswd
-
-    echo "'mha' 사용자가 준비되었습니다."
-fi
+read -p "'mha' 사용자에 사용할 비밀번호를 적어 주세요: " mha_password
+echo "mha:$mha_password" | chpasswd

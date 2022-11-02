35137aae31327700d09d6a0651191a7a3a0b6fd2
Potados <potados99@gmail.com>
2022-11-03 08:30:38 +0900

    chore: 성공 실패 메시지 안내 추가

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -11,7 +11,7 @@ echo "MariaDB를 설치합니다."
 yum -y install mariadb-server
 
 echo "시스템에서 사용할 mha 사용자를 생성합니다."
-useradd -g mysql mha || echo "앗, 'mha' 사용자가 이미 존재하나봅니다!"
+useradd -g mysql mha && echo "mha 사용자를 생성하였습니다." || echo "앗, 'mha' 사용자가 이미 존재하나봅니다!"
 
-read -p "'mha' 사용자에 사용할 비밀번호를 적어 주세요: " mha_password
-echo "mha:$mha_password" | chpasswd
+echo "mha 사용자의 비밀번호를 변경합니다."
+passwd mha

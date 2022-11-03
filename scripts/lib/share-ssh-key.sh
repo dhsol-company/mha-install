29eab99c3f83d897033f720da691a7889696f335
potados99 <potados99@gmail.com>
2022-11-03 09:22:02 +0900

    fix: tty 지정!

--- scripts/lib/share-ssh-key.sh
+++ scripts/lib/share-ssh-key.sh
@@ -7,17 +7,17 @@ set -e
 sudo -i -u mha bash << EOF
 echo "SSH key를 생성합니다."
 echo "모두 Enter로 넘어가주세요 :)"
-ssh-keygen -t rsa -b 4096
+ssh-keygen -t rsa -b 4096 </dev/tty # address terminal directly
 
 echo "만든 SSH key를 db-1 서버에 보냅니다."
 echo "mha 서버의 mha 사용자 비밀번호를 물어볼 것입니다."
-ssh-copy-id mha@db-1
+ssh-copy-id mha@db-1 </dev/tty # address terminal directly
 
 echo "만든 SSH key를 db-1 서버에 보냅니다."
 echo "mha 서버의 mha 사용자 비밀번호를 물어볼 것입니다."
-ssh-copy-id mha@db-2
+ssh-copy-id mha@db-2 </dev/tty # address terminal directly
 
 echo "만든 SSH key를 mha 서버에 보냅니다."
 echo "mha 서버의 mha 사용자 비밀번호를 물어볼 것입니다."
-ssh-copy-id mha@mha
+ssh-copy-id mha@mha </dev/tty # address terminal directly
 EOF

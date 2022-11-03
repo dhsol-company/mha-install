9adffe5b21b9eaa53c179ff39d1f0fa951875c5b
potados99 <potados99@gmail.com>
2022-11-03 09:23:04 +0900

    fix: 출력문 복붙 오타 수정

--- scripts/lib/share-ssh-key.sh
+++ scripts/lib/share-ssh-key.sh
@@ -10,11 +10,11 @@ echo "모두 Enter로 넘어가주세요 :)"
 ssh-keygen -t rsa -b 4096 </dev/tty # address terminal directly
 
 echo "만든 SSH key를 db-1 서버에 보냅니다."
-echo "mha 서버의 mha 사용자 비밀번호를 물어볼 것입니다."
+echo "db-1 서버의 mha 사용자 비밀번호를 물어볼 것입니다."
 ssh-copy-id mha@db-1 </dev/tty # address terminal directly
 
 echo "만든 SSH key를 db-1 서버에 보냅니다."
-echo "mha 서버의 mha 사용자 비밀번호를 물어볼 것입니다."
+echo "db-2 서버의 mha 사용자 비밀번호를 물어볼 것입니다."
 ssh-copy-id mha@db-2 </dev/tty # address terminal directly
 
 echo "만든 SSH key를 mha 서버에 보냅니다."

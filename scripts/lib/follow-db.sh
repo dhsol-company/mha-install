d606c8008898d636927508f98a19234eb1ed982e
potados99 <potados99@gmail.com>
2022-12-30 10:46:05 +0900

    feat: db-2 설정시 슬레이브 설정 진행 여부를 사용자에게 질의

--- scripts/lib/follow-db.sh
+++ scripts/lib/follow-db.sh
@@ -12,10 +12,22 @@ master_host=$2
 
 if [ -z "$slave_host" ] || [ -z "$master_host" ]; then
   echo "첫 번째 인자는 slave의 호스트, 두 번째 인자는 master의 호스트가 필요합니다!"
-  exit 1;
+  exit 1
 fi
 
-echo "이 DB를 slave로 구성하기 위해 db-1의 마스터 정보를 알아 옵니다."
+read -p "$(slave_host)이(가) $(master_host)을(를) 바라보도록 설정하겠습니까? 최초의 설치라면 yes를, 그렇지 않다면 no를 권장합니다. (yes/no) " yn
+
+case $yn in
+yes)
+  echo "네, 진행하겠습니다."
+  ;;
+*)
+  echo "네, 진행하지 않겠습니다. 슬레이브 설정 스크립트를 종료합니다."
+  exit 0
+  ;;
+esac
+
+echo "이 DB($slave_host)를 slave로 구성하기 위해 $(master_host)의 마스터 정보를 알아 옵니다."
 master_log_file=$(mysql --host=$master_host --user=mha --password=mha -Ne "show master status\G" | head -n 2 | tail -n 1)
 master_log_pos=$(mysql --host=$master_host --user=mha --password=mha -Ne "show master status\G" | head -n 3 | tail -n 1)
 
